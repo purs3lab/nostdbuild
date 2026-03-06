ef06b1f39bfeeeaa0ba562823d6f741a1c743425
souragc <souragc2001@gmail.com>
2026-03-06 11:01:50 -0500

    Fix tessts failure

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,11 @@
 #![feature(rustc_private)]
 
-use cargo_test_support::{Project, cargo_test, project};
+use cargo_test_support::{cargo_test, project};
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-use nostd::consts::HIR_VISITOR_SPAN_DUMP;
+use nostd::consts;
 
 fn get_sysroot_lib_path() -> String {
     let output = Command::new("rustc")
@@ -24,7 +24,7 @@ fn get_driver_path() -> std::path::PathBuf {
     std::path::PathBuf::from(env!("CARGO_BIN_EXE_cargo-hir"))
 }
 
-fn run_fixture_test(fixture_name: &str) {
+fn run_fixture_test(fixture_name: &str, fixture_version: &str) {
     let fixture_path = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join("tests/fixtures")
         .join(fixture_name);
@@ -45,12 +45,19 @@ fn run_fixture_test(fixture_name: &str) {
         .env("LD_LIBRARY_PATH", get_sysroot_lib_path())
         .run();
 
-    let actual_json_path = Path::new(HIR_VISITOR_SPAN_DUMP);
+    let actual_json_path = Path::new(consts::RESULTS_PATH).join(format!(
+        "{}-{}-{}",
+        fixture_name,
+        fixture_version,
+        consts::HIR_VISITOR_VISIT_FILE_SUFFIX
+    ));
+        
     let expected_json_path = fixture_path.join("expected.json");
 
     assert!(
         actual_json_path.exists(),
-        "Expected HIR visitor span dump file does not exist. Please ensure that `cargo-hir` ran successfully."
+        "Expected HIR visitor span dump file: {} does not exist. Please ensure that `cargo-hir` ran successfully.",
+        actual_json_path.display()
     );
 
     let actual_json = fs::read_to_string(actual_json_path)
@@ -66,15 +73,15 @@ fn run_fixture_test(fixture_name: &str) {
 
 #[cargo_test]
 fn test_extern_std() {
-    run_fixture_test("test_extern_std");
+    run_fixture_test("test_extern_std", "0.1.0");
 }
 
 #[cargo_test]
 fn vec_direct_usage() {
-    run_fixture_test("vec_direct_usage");
+    run_fixture_test("test_vec_direct_usage", "0.1.0");
 }
 
 #[cargo_test]
 fn test_vec_used_via_use() {
-    run_fixture_test("vec_used_via_use");
+    run_fixture_test("test_vec_used_via_use", "0.1.0");
 }
